42b58a49fb2db921e4167f835b3e33262595f290
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-09-07 18:02:16 +0600

    Add files via upload

---
+++ Lab_1/lab1_6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float C, F;
+    printf("Enter the value of Farenheight:");
+    scanf("%f",&C);
+    C = (5*(F-32)/9);
+    printf("The value of celcious is %.2f\n",C);
+
+}
