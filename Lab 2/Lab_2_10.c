f7a7ed496d7df2e335ad6d0da26909a0df00ae94
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-09-11 21:39:18 +0600

    Add files via upload

---
+++ Lab 2/Lab_2_10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+    int x,s,C=0,Y=0;
+    scanf("%d",&x);
+    s=x%2;
+    if (s==0)
+   { (Y=x+1);
+    printf("%d",Y);
+   }
+    else if(s=!0)
+    {(C=x+2);
+    printf("%d",C);
+    }
+}
