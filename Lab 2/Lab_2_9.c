a525fd4df2c65dfae26fa6b0f23bbecd2ad35c69
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-09-11 21:39:00 +0600

    Add files via upload

---
+++ Lab 2/Lab_2_9.c
@@ -0,0 +1,12 @@
+//9
+
+#include<stdio.h>
+#include<math.h>
+main()
+{
+    int term1,term2;
+    scanf("%d%d",&term1,&term2);
+    if(term1>term2)
+    printf("students got more marks in term 1");
+    else printf("students got more marks in term 2");
+}
