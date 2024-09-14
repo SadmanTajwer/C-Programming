c80977246cd7c88dd6464c6465214ca7ec07b806
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-09-14 18:30:47 +0600

    Add files via upload

---
+++ Lab 3/Lab3.5.c
@@ -0,0 +1,15 @@
+
+//5
+#include<stdio.h>
+#include<math.h>
+int main(){
+int x;
+printf("Enter a number:");
+scanf("%d",&x);
+if(x>0){
+    printf("The number is positive");
+}
+else if(x<0){
+    printf("The number is negative");
+}
+else {printf("The number is 0");}}
