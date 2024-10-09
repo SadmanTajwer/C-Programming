fc3bd421a164dfacf72ae317054ee04b19ea5af3
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-10-09 20:49:45 +0600

    Add files via upload

---
+++ Lab 7 nested loop/Lab 7 nested loop. 4.c
@@ -0,0 +1,6 @@
+Write a C program which can input some numbers in an array and display how many pair are
+possible where both numbers are even
+As example
+Input: 5 4 8 7 6
+Output: 3
+[4 and 8, 4 and 6, 8 and 6]
